bc628dae0ce4b88346c09efe8def1550f9dfb546
Junyu Wang <junyuw@berkeley.edu>
2015-02-24 01:52:09 -0800

    Initial Commit

---
+++ CardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGame
+//
+//  Created by Junyu Wang on 2/24/15.
+//  Copyright (c) 2015 Junyu Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CardGame
+//
+//  Created by Junyu Wang on 2/24/15.
+//  Copyright (c) 2015 Junyu Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
